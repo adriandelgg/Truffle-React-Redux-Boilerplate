8bf1de4d13fcef4449f5705f0a303ceca46bdf99
adriandelgg <adriandelgadoc1@gmail.com>
2021-06-03 00:42:33 -0700

    Fixing client folder

---
+++ client/src/App.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function App() {
+	return <></>;
+}
+
+export default App;
---
+++ client/src/redux/store.js
@@ -0,0 +1,5 @@
+import { configureStore } from '@reduxjs/toolkit';
+
+const store = configureStore({ reducer: {} });
+
+export default store;
